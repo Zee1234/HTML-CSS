4454b110efbe26c0a077b1ee2e2803620c4d452c
Zee1234 <zeeatgit@gmail.com>
2017-04-07 18:01:02 -0500

    Base of compile.lua

--- RiriTamanegi/compile.lua
+++ RiriTamanegi/compile.lua
@@ -1,2 +1,15 @@
-require "luarocks.loader"
-local sass = require "lua-sass-ffi/sass"
+local haml = require "haml"
+local hamlOptions = {format = "html5"}
+local engine = haml.new(hamlOptions)
+local function renderFile(file,vars)
+  engine:render_file(file,vars)
+end
+
+local function sass(file,args)
+  local handle = io.popen("sass "..args.." "..file)
+  local result = handle:read("*a")
+  handle:close()
+  return result
+end
+
+local yaml = require "yaml"
